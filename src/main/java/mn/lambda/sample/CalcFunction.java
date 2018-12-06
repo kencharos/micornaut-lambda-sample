a09a93d0b97abaf3a0cd23b640414486c18f9da5
kencharos <many.front@gmail.com>
2018-12-06 22:29:26 +0900

    fix

--- src/main/java/mn/lambda/sample/CalcFunction.java
+++ src/main/java/mn/lambda/sample/CalcFunction.java
@@ -2,16 +2,16 @@ package mn.lambda.sample;
 
 import io.micronaut.function.FunctionBean;
 
-import javax.inject.Inject;
 import java.util.function.Function;
 
 @FunctionBean("calc")
 public class CalcFunction implements Function<SampleRequest, SampleResponse> {
 
-    @Inject CalculationService service;
-
     @Override
     public SampleResponse apply(SampleRequest req) {
-      return service.calc(req);
+
+        SampleResponse res = new SampleResponse();
+        res.setAnswer(req.getV1() + req.getV2());
+        return res;
     }
 }
